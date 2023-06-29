568cf12fa4a5f05e2566907ff1293f11bb406483
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-06-29 17:16:04 +0700

    Sending Signup Request from Frontend

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,33 +1,76 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useRef, useState } from "react";
+import classes from "./auth-form.module.css";
 
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  async function createUser(email, password) {
+    const response = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || "Something went wrong!");
+    }
+
+    return data;
+  }
+
+  async function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (isLogin) {
+      // log user in
+    } else {
+      try {
+        const result = await createUser(enteredEmail, enteredPassword);
+        console.log(result);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  }
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,36 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { email, password } = data;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message: "Invalid input. Password must be at least 7 characters long.",
+      });
+      return;
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db();
+
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection("users").insertOne({
+      email: email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "Created user!" });
+  }
+}
+
+export default handler;
