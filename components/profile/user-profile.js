2a9eb0fe2dd5fbcd894751f4600d5bbf2018ef76
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-06-30 06:21:32 +0700

    Added Server-Side Guards

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,26 +1,26 @@
-import { getSession } from "next-auth/client";
+// import { getSession } from "next-auth/client";
+// import { useEffect, useState } from "react";
 
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
-import { useEffect, useState } from "react";
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = "/auth";
-      } else {
-        setIsLoading(false);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = "/auth";
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
 
-  // Redirect away if NOT auth
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+  // // Redirect away if NOT auth
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
 
   return (
     <section className={classes.profile}>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,28 @@
-import UserProfile from '../components/profile/user-profile';
+import { getSession } from "next-auth/client";
+
+import UserProfile from "../components/profile/user-profile";
 
 function ProfilePage() {
   return <UserProfile />;
 }
 
 export default ProfilePage;
+
+export async function getServerSideProps(ctx) {
+  const session = await getSession({ req: ctx.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
